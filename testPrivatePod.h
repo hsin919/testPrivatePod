cf1d383f34fdb63f10981e1c5420a81dc66560cc
hsin <hsin919@gmail.com>
2015-02-11 17:53:23 +0800

    First commit for private pod spec

---
+++ testPrivatePod.h
@@ -0,0 +1,16 @@
+//
+//  testPrivatePod.h
+//  testPrivatePod
+//
+//  Created by Nathan on 2015/2/11.
+//  Copyright (c) 2015年 Stupid Pencil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testPrivatePod : NSObject
+
+
++ (NSString *)printVersion;
+
+@end
